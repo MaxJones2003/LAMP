f52c4c51014040d8293325d1b1d3da2df3325060
Max Jones <jones.max2003@gmail.com>
2026-02-06 19:31:20 +0000

    Home Page Style Changes

--- js/contacts.js
+++ js/contacts.js
@@ -42,12 +42,22 @@ function addContact()
 	try
 	{
 		xhr.onreadystatechange = function() 
-		{
-			if (this.readyState == 4 && this.status == 200) 
-			{
-				document.getElementById("contactAddResult").innerHTML = "Contact has been added";
-			}
-		};
+    {
+      if (this.readyState == 4 && this.status == 200) 
+      {
+        document.getElementById("contactAddResult").innerHTML = "Contact has been added";
+        
+        // Reload list so the new contact appears
+        loadContacts();
+        
+        // Optional: clear form fields
+        document.getElementById("firstName").value = "";
+        document.getElementById("lastName").value = "";
+        document.getElementById("phoneNumber").value = "";
+        document.getElementById("email").value = "";
+      }
+    };
+
 		xhr.send(jsonPayload);
 	}
 	catch(err)
@@ -102,6 +112,70 @@ function searchContact()
 	
 }
 
+function addContactToList(contact)
+{
+  const list = document.getElementById("contactList");
+  
+  const row = document.createElement("div");
+  row.className = "contact-row";
+  row.dataset.id = contact.id;
+  
+  row.innerHTML = `
+    <div class="contact-name">${contact.firstName} ${contact.lastName}</div>
+    <div class="contact-email">${contact.email}</div>
+    <div class="contact-phone">${contact.phone}</div>
+    <button class="contact-menu-btn" onclick="openContactMenu(${contact.id})">?</button>
+    `;
+
+  list.appendChild(row);
+}
+
+function loadContacts()
+{
+  let jsonPayload = JSON.stringify({
+    search: "",
+    userId: User.id
+  });
+  
+  let url = urlBase + '/SearchContact' + extension;
+  
+  let xhr = new XMLHttpRequest();
+  xhr.open("POST", url, true);
+  xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+  
+  xhr.onreadystatechange = function ()
+  {
+    if (xhr.readyState === 4 && xhr.status === 200)
+    {
+      let response = JSON.parse(xhr.responseText);
+      
+      const list = document.getElementById("contactList");
+      list.innerHTML = ""; // clear existing list
+      
+      if (!response.results || response.results.length === 0)
+      {
+        list.innerHTML = "<p>No contacts found</p>";
+        return;
+      }
+      
+      for (let c of response.results)
+      {
+        addContactToList({
+          id: c.ID,
+          firstName: c.FirstName,
+          lastName: c.LastName,
+          email: c.Email,
+          phone: c.Phone
+        });
+      }
+    }
+  };
+  
+  xhr.send(jsonPayload);
+}
+
+
+
 function readCookie()
 {
 	User.id = -1;
@@ -131,7 +205,7 @@ function readCookie()
 	}
 	else
 	{
-//		document.getElementById("userName").innerHTML = "Logged in as " + firstName + " " + lastName;
+    //document.getElementById("userName").innerHTML = "Logged in as " + firstName + " " + lastName;
 	}
 }
 
@@ -141,3 +215,40 @@ function exitIfLoggedOut()
     window.location.href = "index.html"; // not logged in
   }
 }
+
+function openContactMenu(contactId)
+{
+  console.log("Open menu for contact:", contactId);
+  // TODO: show edit/delete popup
+}
+
+function editContact(contactId)
+{
+  console.log("Edit contact:", contactId);
+  // TODO: open edit modal
+}
+
+function deleteContact(contactId)
+{
+  console.log("Delete contact:", contactId);
+  // TODO: call DeleteContact API + remove from UI
+}
+
+function openAddContactModal()
+{
+  console.log("Open add contact modal");
+}
+
+function closeAddContactModal()
+{
+  console.log("Close add contact modal");
+}
+
+
+window.onload = function ()
+{
+  readCookie();
+  exitIfLoggedOut();
+  loadContacts();
+};
+
